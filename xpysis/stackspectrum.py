67d31f04dd814b2871c71daba41fbfacb7e745b2
eblur <lia@astro.wisc.edu>
2017-07-24 16:45:14 -0500

    _stack_counts method works

--- xpysis/stackspectrum.py
+++ xpysis/stackspectrum.py
@@ -16,20 +16,21 @@ class StackSpectrum(Spectrum):
         assert len(speclist) > 1, "Need more than one spectrum to stack"
 
         # Modify / overwrite old stuff
-        self.counts       = self._stack_counts(speclist[0], speclist[1:])
+        self.counts       = self._stack_counts(speclist)
         self.exposure     = 1.0  # Pull from ARF, if exposure exists, if not throw a warning and take it from spectrum files
-        self.arf.specresp = self._stack_arf(speclist[0], speclist[1:])
+        self.arf.specresp = self._stack_arf(speclist)
         self.arf.exposure = 1.0
         self.arf.fracexpo = 1.0
 
-    def _stack_counts(self, s0, speclist):
-        counts = s0.counts
-        for s in speclist:
+    def _stack_counts(self, speclist):
+        s0     = speclist[0]
+        counts = np.copy(s0.counts)
+        for s in speclist[1:]:
             assert all(s.bin_lo == s0.bin_lo), "Grids on every spectrum need to match"
             assert all(s.bin_hi == s0.bin_hi), "Grids on every spectrum need to match"
             assert s.bin_unit == s0.bin_unit, "Grids need to be in the same units"
             counts += s.counts
         return counts
 
-    def _stack_arf(self, s0, speclist):
+    def _stack_arf(self, speclist):
         return 0.0
