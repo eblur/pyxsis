7d26d75e8c7a8c61b1990fe6cb04d74e8f769b28
eblur <lia@astro.wisc.edu>
2017-07-26 15:01:44 -0500

    BkgSpectrum basically functions

--- xpysis/__init__.py
+++ xpysis/__init__.py
@@ -2,3 +2,4 @@
 from plot import *
 from binspectrum import *
 from stackspectrum import *
+from bkgspectrum import *
--- xpysis/bkgspectrum.py
+++ xpysis/bkgspectrum.py
@@ -1,10 +1,13 @@
 import os
 import numpy as np
+from astropy.io import fits
 import clarsach
 
 KEV  = ['kev', 'keV']
 ANGS = ['Angstroms','Angstrom','Angs','angstroms','angstrom','angs']
 ALLOWED_UNITS = KEV + ANGS
+
+ALLOWED_TELESCOPES = ['HETG','ACIS','other']
 
 __all__ = ['BkgSpectrum']
 
@@ -12,15 +15,20 @@ class BkgSpectrum(object):
     """
     Class for reading in background spectra
     """
-    def __init__(self, filename):
-        self.__store_path(filename)
+    def __init__(self, filename, telescope='HETG'):
+        if telescope == 'HETG':
+            self._read_HETG(filename)
+        else:
+            self._read_other(filename)
 
-    def _read_chandra(self, filename):
+    def _read_HETG(self, filename):
         this_dir = os.path.dirname(os.path.abspath(filename))
         ff   = fits.open(filename)
-        self.data = ff[1].data
-        self.rmf_file = this_dir + "/" + ff[1].header['RESPFILE']
-        self.arf_file = this_dir + "/" + ff[1].header['ANCRFILE']
-        self.rmf = clarsach.RMF(self.rmf_file)
-        self.arf = clarsach.ARF(self.arf_file)
+        data   = ff[1].data
+        self.hdr    = ff[1].header
+        self.data   = data
+        self.counts = data['BACKGROUND_UP'] + data['BACKGROUND_DOWN']
+        self.bin_lo = data['BIN_LO']
+        self.bin_hi = data['BIN_HI']
+        self.bin_unit = data.columns['BIN_LO'].unit
         ff.close()
