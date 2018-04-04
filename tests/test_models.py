c2b0a5f86b3e8df5828e3722261815f7fd67430f
Lia Corrales <lia@astro.wisc.edu>
2018-04-03 20:10:47 -0500

    Wrote test to assert parameter limits

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,5 +8,8 @@ def test_model_superclass():
     lims  = [(0,10), (0,10), (0,10)]
     units = ['s', 'cm^2', 'keV']
     m = Model(names, vals, lims, units)
+    # Check that __getitem__ works
     for k in names:
         assert isinstance(m[k], u.Quantity)
+    # Check that initializing with an out-of-bound parameter does not work
+    assert Model(names, [-10., 2.0, 3.0], lims, units) is None
--- xpysis/models/model.py
+++ xpysis/models/model.py
@@ -10,12 +10,33 @@ class Model(object):
         par_lims : list of tuples containing parameter limits
         par_units : list of parameter units (strings)
         """
+        # Check that the parameter values fall within the limits
+        # If they don't, return None
+        for v,l in zip(par_vals, par_lims):
+            assert self._check_lims(v, l), return
         self.keys = par_keys
         self.vals = dict(zip(par_keys, par_vals))
-        self.lims = dict(zip(par_keys, par_vals))
+        self.lims = dict(zip(par_keys, par_lims))
         self.units = dict(zip(par_keys, par_units))
         return
+
+    def _check_lims(self, val, lim):
+        result = (val >= lim[0]) & (val <= lim[1])
+        return result
 
     def __getitem__(self, key):
         assert key in self.keys, print("{} not a valid parameter key".format(key))
         return u.Quantity(self.vals[key], self.units[key])
+
+    def update_par(self, keys, new_vals):
+        # Finds the nearest limit
+        def find_nearest_lim(v, l):
+            if (v < l[0]): return l[0]
+            if (v > l[1]): return l[1]
+        # Set the new parameters
+        for k,v in zip(keys, new_vals):
+            if self._check_lims(v, self.lims[k]):
+                self.vals[k] = v
+            else:
+                self.vals[k] = find_nearest_lim(v, self.lims[k])
+        return
