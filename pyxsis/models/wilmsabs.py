23a24bd837175d40ba0baa4725462b0089e846cf
Lia Corrales <lia@astro.wisc.edu>
2018-04-05 11:35:18 -0700

    First pass and Wilms absorption model (tbnew)

---
+++ pyxsis/models/wilmsabs.py
@@ -0,0 +1,22 @@
+import os
+from astropy.table import Table
+from scipy.interpolate import interp1d
+from .model import model
+
+def _find_tablefile(name):
+    root_path = os.path.dirname(__file__)
+    data_path = root_path + 'tables/'
+    return data_path + name
+
+class WilmsAbs(Model):
+    def __init__(self, nH=1.e22, lims=[(0, 1.e24)], units=['cm^-2']):
+        Model.__init__(self, ['nH'], [nH], lims, units)
+        self.xsect = self._read_xsect()
+        return
+
+    def _read_xsect(self):
+        fname = _find_tablefile('cosplot_table.txt')
+        tt = Table.read(fname, format='ascii')
+        return interp1d(tt['col1'], tt['col2'])
+
+    #def calculate(self, ener_lo, ener_hi):
