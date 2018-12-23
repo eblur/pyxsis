601541bf5a14d42f610b6ba74744a8a82b481cdf
Lia Corrales <liac@umich.edu>
2018-12-22 20:37:45 -0500

    plot_unfold works again

--- pyxsis/plot.py
+++ pyxsis/plot.py
@@ -31,19 +31,17 @@ def plot_counts(ax, spectrum, xunit='keV', perbin=True, rate=False, \
     ax.errorbar(mid.value, y.value, yerr=yerr.value,
                 ls='', markersize=0, color='k', capsize=0, alpha=0.5)
     ax.step(mid, y, where='mid', **kwargs)
-    ax.set_xlabel(mid.unit)
-    ax.set_ylabel(y.unit)
+    ax.set_xlabel(mid.unit.to_string(format='latex_inline'))
+    ax.set_ylabel(y.unit.to_string(format='latex_inline'))
     return
 
 
 def plot_unfold(ax, spectrum, xunit='keV', perbin=False, \
                 bkgsub=True, usebackscal=True, **kwargs):
 
-    assert isinstance(spectrum, binspectrum.Spectrum)
-
     # Models will always be in keV bin units
     no_mod  = np.ones_like(spectrum.arf.specresp)  # a non-model of ones (integrated)
-    eff_tmp = spectrum.apply_resp(no_mod)
+    eff_tmp = spectrum.apply_response(no_mod)
 
     def _bin_exp(exp, binning):
         # Use mean effective exposure for binned spectra
@@ -52,37 +50,42 @@ def plot_unfold(ax, spectrum, xunit='keV', perbin=False, \
         assert len(result) == (nend - nstart + 1)
         return np.array(result)
 
-    # Now deal with desired xunit
-    assert xunit in ALLOWED_UNITS
-    lo, hi, cts, cts_err = spectrum.bin_counts(xunit, bkgsub=bkgsub, usebackscal=usebackscal)
-    mid = 0.5 * (lo + hi)
+    # Get the binned counts
+    lo, hi, cts, cts_err = spectrum.binned_counts(bkgsub=bkgsub, usebackscal=usebackscal)
+
+    xlo = lo.to(u.Unit(xunit), equivalencies=u.spectral())
+    xhi = hi.to(u.Unit(xunit), equivalencies=u.spectral())
+    mid = 0.5 * (xlo + xhi)
+
+    # Get the binned effective area
     if all(spectrum.binning == 0):
         eff_exp = eff_tmp[spectrum.notice]
     else:
         eff_exp = _bin_exp(eff_tmp[spectrum.notice], spectrum.binning[spectrum.notice])
+    eff_exp *= u.cm**2 * u.ct # cm^2 ct / phot
 
-    flux, f_err = np.zeros_like(eff_exp), np.zeros_like(eff_exp)
+    # Calculate photon flux from binned effective area
+    #flux, f_err = np.zeros_like(eff_exp), np.zeros_like(eff_exp)
     ii = np.isfinite(eff_exp) & (eff_exp != 0.0)
-    if xunit in ANGS:
-        flux[ii] = cts[ii] / eff_exp[ii][::-1]
-        f_err[ii] = cts_err[ii] / eff_exp[ii][::-1]
-    else:
-        flux[ii] = cts[ii] / eff_exp[ii]
-        f_err[ii] = cts_err[ii] / eff_exp[ii]
+    flux = cts[ii] / eff_exp[ii] / spectrum.exposure # phot / cm^2 / second
+    ferr = np.sqrt(cts[ii].value)*u.ct / eff_exp[ii] / spectrum.exposure
 
     if perbin:
         dbin   = 1.0
-        ylabel = 'Flux [phot cm$^{-2}$ s$^{-1}$ bin$^{-1}$]'
     else:
-        dbin   = hi - lo
-        ylabel = 'Flux [phot cm$^{-2}$ s$^{-1}$ %s$^{-1}$]' % xunit
+        dbin   = np.abs(xhi - xlo)[ii]
+
+    # plot values
+    x = mid[ii]
+    y = flux / dbin
+    yerr = ferr / dbin
 
     # Now plot it
-    ax.errorbar(mid, flux/dbin, yerr=f_err/dbin,
+    ax.errorbar(x.value, y.value, yerr=yerr.value,
                 ls='', marker=None, color='k', capsize=0, alpha=0.5)
-    ax.step(lo, flux/dbin, where='mid', **kwargs)
-    ax.set_xlabel("%s" % xunit)
-    ax.set_ylabel(ylabel)
+    ax.step(x, y, where='mid', **kwargs)
+    ax.set_xlabel(x.unit.to_string(format='latex_inline'))
+    ax.set_ylabel("phot {}".format(y.unit.to_string(format='latex_inline')))
 
 def plot_model_flux(ax, spectrum, model, xunit='keV', perbin=False, **kwargs):
 
