3221c0ce44793cfebbccde93360495c95326cfe5
eblur <liac@umich.edu>
2019-09-30 22:59:30 -0400

    fixed units on Gaussian result

--- pyxsis/models/gaussian.py
+++ pyxsis/models/gaussian.py
@@ -51,4 +51,5 @@ class Gaussian(Model):
         ecenter = self['ecenter']
         sigma   = self['sigma']
         emid    = 0.5 * (ener_lo + ener_hi) * u.keV
-        return norm * np.exp( -0.5 * (emid - ecenter)**2 / sigma**2 )
+        de      = ener_hi - ener_lo
+        return norm * np.exp( -0.5 * (emid - ecenter)**2 / sigma**2 ) * de # flux per bin
